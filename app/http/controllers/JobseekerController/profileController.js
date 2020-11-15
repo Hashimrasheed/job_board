31da8fb27f8d28d7f5e7ee68459b8173788e8e9e
Hashimrasheed <hashimrasheed1999@gmail.com>
2020-11-15 21:05:30 +0530

    fixed isue in user profile

--- app/http/controllers/JobseekerController/profileController.js
+++ app/http/controllers/JobseekerController/profileController.js
@@ -13,7 +13,7 @@ function profileController() {
         editProfile(req, res) {
             res.render('user/profileEdit')
         },
-        postEditProfile(req, res) {
+        async postEditProfile(req, res) {
             const user = {
                 name: req.body.fname + " " + req.body.lname,
                 mobile: req.body.mobile,
@@ -23,15 +23,23 @@ function profileController() {
                 location: req.body.location,
                 skills: req.body.skills,
                 education: req.body.education,
-                experience: req.body.experience
+                experience: req.body.experience,
+                profilePic: true
             }
+            profilePic = req.files.profilePic
             const userId = new ObjectID(req.body.id)
-
-            db.get().collection(collection.USERS).updateOne({_id: userId}, {$set: user}, (data) => {
-                // console.log(data);
-                // req.session.user = data;
-                res.redirect(`/user/resumeupload`)
+            await db.get().collection(collection.USERS).updateOne({_id: userId}, {$set: user})
+            const usersession = await db.get().collection(collection.USERS).findOne({_id: userId})
+            profilePic.mv(`public/images/userProfile/${userId}.jpg`, (err) => {
+                if(!err) {
+                    req.session.user = usersession;
+                    res.redirect(`/user/resumeupload`)
+                } else {
+                    console.log(err);
+                }
             })
+            
+            
         },
         resumeUpload(req, res) {
             res.render('user/resumeupload')
@@ -42,6 +50,7 @@ function profileController() {
             await db.get().collection(collection.USERS).updateOne({_id: userId}, {$set: {resume: true}}, () => {
                 resume.mv(`public/images/userResume/${userId}.pdf`, (err) => {
                     if(!err) {
+                        
                         res.redirect(`/user/profile/${userId}`)
                     } else {
                         console.log(err);
